3ccc4cbd6c3d429a4c1e75aae7a9d75be51070cb
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-28 21:31:26 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -28,10 +28,7 @@ variable "app_version" {
 variable "component" {
   type = string
 }
-variable "port_number" {
-  type = number
-  default = 8080
-}
+
 
 variable "health_check_path" {
   default = "/health"
