7a2dd690778e667972b021e2ed8954efe5656cb2
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-31 04:00:50 +0530

    terraform

--- main.tf
+++ main.tf
@@ -203,11 +203,11 @@ resource "terraform_data" "main_delete" {
   triggers_replace = [
     aws_instance.main.id
   ]
-    depends_on = [aws_autoscaling_policy.main]
+  depends_on = [aws_autoscaling_policy.main]
 
-    # here local means terraform excuted in bastaion
-    provisioner "local-exec" {
-       command = "aws ec2 terminate-instances --instance-ids ${aws_instance.mani.id}"
+  # here local means terraform excuted in bastaion
+  provisioner "local-exec" {
+      command = "aws ec2 terminate-instances --instance-ids ${aws_instance.mani.id}"
 
-    }
+  }
 }
