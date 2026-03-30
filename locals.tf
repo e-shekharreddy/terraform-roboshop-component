f900531bfaa012e7a6de81ab3a30cd69d286b221
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-31 05:20:08 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -6,7 +6,7 @@ locals {
 
     }
     ami_id = data.aws_ami.joindevops.id
-    private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     sg_id = data.aws_ssm_parameter.sg_id.value
     health_check_path = var.component == "frontend" ? "/" : "/health"
